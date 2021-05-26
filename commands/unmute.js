29f97732cbc27d0437dba1006d53bb66f70889aa
FranekWadas <franiowadas@gmail.com>
2021-05-26 10:24:38 +0200

    unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -11,10 +11,27 @@ module.exports = {
 
             var id = args[0].replace(/[\\<>@#&!]/g, "");
 
-            var user = message.guild.members.cache.get();
+            var user = message.guild.members.cache.get(id);
 
             if (typeof user !== 'undefined') {
+                if (typeof punishments.mutes.find(m => m.userId == id) !== 'undefined') {
+                    
+                    var mute = punishments.mutes.find(m => m.userId == id);
 
+                    try {
+                        punishments.mutes.splice(punishments.mutes.indexOf(mute), 1);
+                        client.updateConfig();
+                        message.channel.send(`**Odciszyłem gracza:** *__${user.user.username}__*`);
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                    user.roles.remove('841617507168288798');
+
+                } else {
+                    message.channel.send("**Ten gracz nie jest wyciszony!**");
+                    return;
+                }
             } else {
                 message.channel.send("**Podaj prawidłową nazwę gracza!**");
             }
