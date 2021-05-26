faefb2daf2c25ccc11e7eb0bb0852aec3fe3a740
Dewidos <daw.po@wp.pl>
2021-05-26 11:32:47 +0200

    Added authentication to mute command

--- commands/mute.js
+++ commands/mute.js
@@ -7,6 +7,26 @@ module.exports = {
 
     execute(message, args, client) {
 
+        var config = client.configFile.find(c => c.guildId == message.guild.id);
+
+        if (config.moderatorRoles.length <= 0) {
+            message.channel.send("Błąd konfiguracji! Nikt nie skonfigurował roli moderatorskich.");
+            return;
+        }
+
+        var hasPermission = false;
+
+        config.moderatorRoles.forEach(role => {
+            if (message.member.roles.cache.has(role)) {
+                hasPermission = true;
+            }
+        });
+
+        if (!hasPermission) {
+            message.channel.send("Nie masz wystarczających uprawnień!");
+            return;
+        }
+        
         var punishments = client.punishments.find(e => e.guildId == message.guild.id);
 
         if (typeof args[0] !== 'undefined' && args[0] != "") {
