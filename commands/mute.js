b6d2b2ddc3af934c81c7887a911fdb3f1754c6aa
FranekWadas <franiowadas@gmail.com>
2021-05-26 11:24:34 +0200

    mute.js

---
+++ commands/mute.js
@@ -0,0 +1,87 @@
+const Discord = require('discord.js')
+
+module.exports = {
+
+    "name": "mute",
+    "description": "wycisz gracza na nieograniczony czas",
+
+    execute(message, args, client) {
+
+        var punishments = client.punishments.find(e => e.guildId == message.guild.id);
+
+        if (typeof args[0] !== 'undefined' && args[0] != "") {
+
+            if (typeof args[1] !== 'undefined' || args[1] == "") {
+                
+                var reason = "";
+
+                var length = args.length - 1;
+    
+                for (let i = 1; i <= length; i++) {
+                    reason = reason + args[i];
+                    if (i != length) reason = reason + " ";
+                }
+                
+                var id = args[0].replace(/[\\<>@#&!]/g, "");
+                var player = message.guild.members.cache.get(id);
+
+                if (typeof player !== 'undefined') {
+                    
+                    var embed = new Discord.MessageEmbed()
+                    .setColor('#34c6eb')
+                    .setTitle("Wykonano!")
+                    .setDescription(`**Wyciszyłem gracza o nicku: **<@${id}>\n\n**Za: **__${reason}__`)
+                    .setFooter("Polecam się na przyszłość!");
+
+                    message.channel.send(embed);
+                    try {
+                        punishments.mutes.push({
+                            "userId": id,
+                            "issuerId": message.author.id,
+                            "reason": reason,
+                        });
+    
+                        player.roles.add('841617507168288798');
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    
+
+                } else {
+                    message.channel.send("**Podaj prawidłową nazwę gracza!**");
+                }
+            } else {
+
+                var id = args[0].replace(/[\\<>@#&!]/g, "");
+                var player = message.guild.members.cache.get(id);
+
+                if (typeof player !== 'undefined') {
+                    
+                    var embed = new Discord.MessageEmbed()
+                    .setColor('#34c6eb')
+                    .setTitle("Wykonano!")
+                    .setDescription(`**Wyciszyłem gracza o nicku: **<@${id}>\n\n**Za: **__Powód nie został określony__`)
+                    .setFooter("Polecam się na przyszłość!");
+
+                    message.channel.send(embed);
+                    try {
+                        punishments.mutes.push({
+                            "userId": id,
+                            "issuerId": message.author.id,
+                            "reason": "Powód nie został określony",
+                        });
+    
+                        player.roles.add('841617507168288798');
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                }
+            }
+        } else {
+            message.channel.send("**Musisz podać nazwe gracza, krórego chcesz wyciszyć!**")
+        }
+
+    }
+
+}
