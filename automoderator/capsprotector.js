b7989f5e109699e472b10baede7cab8e1ec44497
Dewidos <daw.po@wp.pl>
2021-05-27 12:21:15 +0200

    First test of CapsProtector

--- automoderator/capsprotector.js
+++ automoderator/capsprotector.js
@@ -1,3 +1,17 @@
 module.exports = (message, client) => {
+    var config = client.configFile.find(c => c.guildId == message.guild.id);
+    var punishments = client.punishments.find(p => p.guildId == message.guild.id);
+    
+    var msgContent = message.content;
+    msgContent.toLowerCase();
 
+    var counter = 0;
+    for (const key of msgContent) {
+        var index = msgContent.indexOf(key);
+
+        if (message.content[index] != key) counter++;
+        else counter = 0;
+    }
+
+    console.log("Liczba capsów: " + counter);
 };
