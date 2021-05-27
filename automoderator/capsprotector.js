11ded3ed8a6bddc57b07f9c02b429d712f30a413
Dewidos <daw.po@wp.pl>
2021-05-27 13:10:24 +0200

    Done capsprotector

--- automoderator/capsprotector.js
+++ automoderator/capsprotector.js
@@ -1,6 +1,23 @@
+const warnScript = require('./../commands/warn');
+
 module.exports = (message, client) => {
     var config = client.configFile.find(c => c.guildId == message.guild.id);
+
+    if (typeof config === 'undefined' || typeof config.autoModConfig === 'undefined') {
+        console.error("Błąd konfiguracji automoda!");
+        return;
+    }
+
+    var protectorConfig = config.autoModConfig.find(c => c.name == "CapsProtector");
+
+    if (typeof protectorConfig === 'undefined') return;
+
     var punishments = client.punishments.find(p => p.guildId == message.guild.id);
+
+    if (typeof punishments === 'undefined') {
+        console.error("Błąd konfiguracji kar!");
+        return;
+    }
     
     var msgContent = message.content;
     msgContent = msgContent.toLowerCase();
@@ -19,4 +36,33 @@ module.exports = (message, client) => {
     }
 
     console.log("Liczba capsów: " + maxCapsCount);
+
+    try {
+        
+        var player = message.member;
+        
+        var warnCapsCount = 6;
+
+        if (typeof protectorConfig.capsLetterCount !== 'undefined') warnCapsCount = protectorConfig.capsLetterCount;
+
+        if (maxCapsCount >= warnCapsCount) {
+            punishments.warnings.push({
+                "userId": message.author.id,
+                "issuerId": '844926717084041238',
+                "warnId": punishments.nextWarnId,
+                "reason": "CapsProtector automod module"
+            });
+    
+            var nextId = parseInt(punishments.nextWarnId);
+            nextId++;
+            punishments.nextWarnId = nextId.toString();
+
+            warnScript.giveWarnRole(punishments.warnings.filter(w => w.userId == id).length, player, config.warningRoles);
+            client.updateConfig();
+        }
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 };
