77f5dde0e31b9071e96e1d81b2bc1fc9370556c7
FranekWadas <franiowadas@gmail.com>
2021-05-29 22:02:45 +0200

    szybki test

--- commands/slub.js
+++ commands/slub.js
@@ -8,19 +8,45 @@ module.exports = {
 
         if (typeof args[0] !== 'undefined' && args[0] != "") {
 
+            const yes = '✅';
+            const no = '❌'
+
             var id = args[0].replace(/[\\<>@#&!]/g, "");
 
             var user = message.guild.members.cache.get(id);
 
             if (typeof user !== 'undefined') {
                 
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8B0000')
-                .setTitle(`Gracz: ${message.author.username} ci sie oświadczył!`)
-                .setDescription(`**Czy ty: <@${id}>, wyjdziesz za: ${message.author.username}?**`)
-                .setFooter("Wybierz emotke: ✅ oznacza ze sie zgadzasz, ❌ oznacza że nie.")
+                let embed = new Discord.MessageEmbed()
+                    .setColor('#8B0000')
+                    .setTitle(`Gracz: ${message.author.username} ci sie oświadczył!`)
+                    .setDescription(`**Czy ty: <@${id}>, wyjdziesz za: ${message.author.username}?**`)
+                    .setFooter("Wybierz emotke: ✅ oznacza ze sie zgadzasz, ❌ oznacza że nie.")
 
-                user.send(embed);
+                let MessageEmbed = await user.send(embed);
+                MessageEmbed.react(yes);
+                MessageEmbed.react(no);
+
+                client.on('messageReactionAdd', async (reaction, user) =>{
+
+                    if (reaction.message.partial) await reaction.message.fetch();
+                    if (reaction.partial) await reaction.fetch();
+                    if (user.bot) return;
+                    if (!reaction.message.guild) return;
+
+                    if (user.id = id) {
+                        if (reaction.name === yes) {
+                            message.channel.send("cos");
+                        } else if (reaction.name == no){
+
+                        } else {
+                            return;
+                        }
+                    } else {
+                        return
+                    }
+                });
+
 
             } else {
                 message.channel.send("**Podaj poprawny nick/id!**");
