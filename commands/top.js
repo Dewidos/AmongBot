4ce3be6a8bbefb82a4daa020771f6ac348356bff
Dewidos <daw.po@wp.pl>
2021-05-29 12:38:21 +0200

    Trying to make bubble sort

--- commands/top.js
+++ commands/top.js
@@ -5,22 +5,31 @@ module.exports = {
 
     execute(message, args, client) {
 
-        if (typeof args[0] === 'undefined' || args[0] == "") {
+        var rank = client.rank.find(e => e.guildID == message.guild.id);
 
-            var rank = client.rank.find(e => e.guildID == message.guild.id);
+        if (typeof rank === 'undefined') {
 
-            if (typeof rank === 'undefined') {
-
-                message.channel.send("**Wystąpił błąd konfiguracji! Prawdopodobnie nie istnieje plik rank.json**");
-                return;
-            }
-
-
-        } else {
-
-            message.channel.send("**Ta komenda nie wymaga żadnego argumentu!**");
-
+            message.channel.send("**Wystąpił błąd konfiguracji! Prawdopodobnie nie istnieje plik rank.json**");
+            return;
         }
+
+        var sortTest = [2, 4, 4, 9, 1, 4];
+
+        for (let i = 0; i < sortTest.length; i++) {
+            for (let j = sortTest.length - 1; j >= 0; j--) {
+                const index = sortTest.indexOf(xpObject);
+
+                let firstXp = xpObject;
+                let secondXp = sortTest[index - 1];
+
+                if (firstXp > secondXp) {
+                    sortTest[index] = secondXp;
+                    sortTest[index - 1] = firstXp;
+                }
+            }
+        }
+
+        message.channel.send(sortTest);
 
     }
 
