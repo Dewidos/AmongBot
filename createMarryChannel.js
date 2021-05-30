31dee1d466ea9c71493dd51cbcf4b1a2d9d27e25
Dewidos <daw.po@wp.pl>
2021-05-30 13:45:16 +0200

    Testing marriages

--- createMarryChannel.js
+++ createMarryChannel.js
@@ -1,3 +1,21 @@
 module.exports = (user1, user2, guild, client) => {
-    
+    guild.channels.create(`slub-${user1.user.username}-${user2.user.username}`, {
+        type: 'text',
+        permissionOverwrites: [
+            {
+                id: guild.roles.everyone.id,
+                deny: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+            },
+            {
+                id: user1.id,
+                allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+            },
+            {
+                id: user2.id,
+                allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+            }
+        ]
+    });
+
+    console.log("Created slub channel");
 };
