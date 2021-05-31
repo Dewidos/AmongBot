118c3d2d694a8530566ac71b7dea3f60e0f65557
Dewidos <daw.po@wp.pl>
2021-05-31 17:15:35 +0200

    Made bot tell you hour

--- dialogs/hour.js
+++ dialogs/hour.js
@@ -1,3 +1,10 @@
 module.exports = (message, client) => {
-    message.channel.send("Zatrybiłem!");
+    var messageToSend = "Mamy teraz ";
+
+    var date = new Date();
+    var time = date.toTimeString();
+
+    messageToSend = messageToSend + `**${time}**.`;
+
+    message.channel.send(messageToSend);
 };
