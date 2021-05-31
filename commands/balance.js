6c09a97cf0860151f081e9ed696b6c35a81928f8
Dewidos <daw.po@wp.pl>
2021-05-31 19:58:54 +0200

    Added economy

---
+++ commands/balance.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    "name": "bal",
+    "description": "Sprawdź ile masz pieniędzy!",
+    execute(message, args, client) {
+        var economy = client.economy.find(e => e.guildId == message.guild.id);
+
+        if (typeof economy === 'undefined') {
+            message.channel.send("Wystąpił błąd konfiguracji ekonomii! Skontaktuj się z administracją.");
+            return;
+        }
+
+        if (typeof args[0] === 'undefined' || args[0] == "") {
+            var id = message.author.id;
+            var user = message.member;
+        } else {
+            var id = args[0].replace(/[\\<>@#&!]/g, "");
+            var user = message.guild.members.cache.get(id);
+        }
+
+        if (typeof user === 'undefined') {
+            message.channel.send("Wystąpił nieznany błąd! Spróbuj ponownie później, powiadom developera o problemie.");
+            return;
+        }
+
+        var userBalance = economy.playerBalance.find(b => b.playerId == id);
+
+        if (typeof userBalance === 'undefined') {
+            var walletMoney = 0;
+            var bankMoney = 0;
+        } else {
+            var walletMoney = userBalance.walletMoney;
+            var bankMoney = userBalance.bankMoney;
+        }
+
+        var embed = new Discord.MessageEmbed()
+        .setColor('#34c6eb')
+        .setTitle(`Majątek gracza ${user.user.username}`)
+        .setDescription(`**Pieniądze w portfelu:** ${walletMoney} ${economy.currency.symbol}\n**Pieniądze w banku:** ${bankMoney} ${economy.currency.symbol}`)
+        .setFooter("Polecam się na przyszłość :)");
+        
+        message.channel.send(embed);
+    }
+};
---
+++ commands/wplac.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    "name": "wplac",
+    "description": "Wpłać pieniądze do banku.",
+    execute(message, args, client) {
+        var economy = client.economy.find(e => e.guildId == message.guild.id);
+
+        if (typeof economy === 'undefined') {
+            message.channel.send("Wystąpił błąd konfiguracji ekonomii! Skontaktuj się z administracją.");
+            return;
+        }
+
+        var id = message.author.id;
+        var user = message.member;
+
+        var userBalance = economy.playerBalance.find(b => b.playerId == id);
+
+        if (typeof userBalance === 'undefined') {
+            message.channel.send(`Nie masz jeszcze żadnych pieniędzy! Użyj na przykład komendy **${client.prefix}pracuj** aby zdobyć swoje pierwsze pieniądze!`);
+            return;
+        }
+
+        var walletMoney = userBalance.walletMoney;
+        var bankMoney = userBalance.bankMoney;
+
+        if (walletMoney <= 0) {
+            message.channel.send(`Nie masz żadnych środków do wpłaty! Aby podejrzeć swój majątek, użyj komendy **${client.prefix}balance**.`);
+            return;
+        }
+
+        if (typeof args[0] === 'undefined' || args[0] == "") {
+            var moneyToTransfer = walletMoney;
+        } else {
+            try {
+                var moneyToTransfer = parseInt(args[0]);
+            } catch (error) {
+                message.channel.send("Podano zły format ilości pieniędzy do wpłaty!");
+                console.error(error);
+                return;
+            }
+        }
+
+        userBalance.bankMoney += moneyToTransfer;
+        userBalance.walletMoney -= moneyToTransfer;
+
+        var embed = new Discord.MessageEmbed()
+        .setColor('#34c6eb')
+        .setTitle(`Udało się!`)
+        .setDescription(`Wpłacono **${moneyToTransfer} ${economy.currency.symbol}** na konto w banku!`)
+        .setFooter("Polecam się na przyszłość :)");
+
+        message.channel.send(embed);
+    }
+};
