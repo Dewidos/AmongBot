a121cba4965ebee15d8506bc46b838bd5c2144f8
FranekWadas <franiowadas@gmail.com>
2021-06-02 14:37:14 +0200

    cos tam jeszcze raz

--- speakingToBot.js
+++ speakingToBot.js
@@ -5,7 +5,47 @@ module.exports = (message, client, channelToSpeakId, StringPrefix) => {
     if (message.channel.id != channelToSpeakId || message.author.bot) return;
 
     const messageContent = message.content.toLowerCase();
+    messageContent = messageContent.replace(/[\\<>@#&!?]/g, "");
+
+module.exports = (message, client, channelToSpeakId, StringPrefix) => {
+
+    if (message.channel.id != channelToSpeakId || message.author.bot) return;
+
+    const messageContent = message.content.toLowerCase();
     
+    var forFun = client.forFun.find(d => d.guildID == message.guild.id);
+    var array = new Array();
+
+    var functionObject = forFun.dialogs.find(d => d.message.includes(messageContent));
+    
+    if (!functionObject) {
+        message.channel.send("Przepraszam, ale nie rozumiem :(");
+        
+        return;
+    }
+
+    if (message.content === "pls meme") {
+      return;
+    }
+
+    if (functionObject.functionName.startsWith(StringPrefix)) {
+
+      var thisString = functionObject.functionName;
+
+      var messageToSend = thisString.slice(StringPrefix.length);
+
+      message.channel.send(messageToSend);
+
+      return;
+
+    }
+
+    var actionFunction = client.speakFunctions[functionObject.functionName];
+
+    actionFunction(message, client);
+
+    client.lastExecutedSpeakingAction = functionObject;
+}
     var forFun = client.forFun.find(d => d.guildID == message.guild.id);
     var array = new Array();
 
