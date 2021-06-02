6549a931a5fa23414fe93148c6a95c5664ef5db7
Dewidos <>
2021-06-02 17:47:39 +0000

    Repl.it upload 2

--- dialogs/repeatMe.js
+++ dialogs/repeatMe.js
@@ -2,6 +2,8 @@ module.exports = (message, client) =>  {
 
   var forFun = client.forFun.find(d => d.guildID == message.guild.id);
 
+  messageContent
+
   var prefix = forFun.dialogs.find(d => d.message.includes(messageContent));
 
 }
