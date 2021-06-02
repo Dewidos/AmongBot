be185d7fcc223ca395ef5704c24f7b637c31a1d1
Global Game Game <>
2021-06-02 13:10:08 +0000

    commit

--- speakingToBot.js
+++ speakingToBot.js
@@ -39,36 +39,4 @@ module.exports = (message, client, channelToSpeakId, StringPrefix) => {
 
     client.lastExecutedSpeakingAction = functionObject;
 
-    var forFun = client.forFun.find(d => d.guildID == message.guild.id);
-    var array = new Array();
-
-    var functionObject = forFun.dialogs.find(d => d.message.includes(messageContent));
-    
-    if (!functionObject) {
-        message.channel.send("Przepraszam, ale nie rozumiem :(");
-        
-        return;
-    }
-
-    if (message.content === "pls meme") {
-      return;
-    }
-
-    if (functionObject.functionName.startsWith(StringPrefix)) {
-
-      var thisString = functionObject.functionName;
-
-      var messageToSend = thisString.slice(StringPrefix.length);
-
-      message.channel.send(messageToSend);
-
-      return;
-
-    }
-
-    var actionFunction = client.speakFunctions[functionObject.functionName];
-
-    actionFunction(message, client);
-
-    client.lastExecutedSpeakingAction = functionObject;
 }
