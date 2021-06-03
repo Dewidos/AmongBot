e8aad89e4d6fbcc1a423100a0c47e0a63aaad207
Dewidos <daw.po@wp.pl>
2021-06-03 21:09:39 +0200

    Polecam.js refactor

--- commands/polecam.js
+++ commands/polecam.js
@@ -12,100 +12,87 @@ module.exports = {
         var config = client.configFile.find(c => c.guildId == message.guild.id);
 
         if (typeof args[0] !== 'undefined') {
+            message.channel.send("Podaj prawidłowy nick admina, którego chcesz polecić!");
+            return;
+        }
 
-            var id = args[0].replace(/[\\<>@#&!]/g, "");
+        var id = args[0].replace(/[\\<>@#&!]/g, "");
 
-            if (config.moderatorRoles.length <= 0) {
-                message.channel.send("Błąd konfiguracji! Nikt nie skonfigurował roli moderatorskich.");
-                return;
+        if (config.moderatorRoles.length <= 0) {
+            message.channel.send("Błąd konfiguracji! Nikt nie skonfigurował roli moderatorskich.");
+            return;
+        }
+
+        var hasPermission = false;
+
+        config.moderatorRoles.forEach(role => {
+            if (message.guild.members.cache.get(id).roles.cache.has(role)) {
+                hasPermission = true;
             }
-    
-            var hasPermission = false;
-    
-            config.moderatorRoles.forEach(role => {
-                if (message.guild.members.cache.get(id).roles.cache.has(role)) {
-                    hasPermission = true;
-                }
-            });
+        });
 
-            if (!hasPermission) {
-                message.channel.send("Ta osoba nie należy do administracji!");
-                return;
+        if (!hasPermission) {
+            message.channel.send("Ta osoba nie należy do administracji!");
+            return;
+        }
+
+        if (message.channel.id != config.polecAdminaChannelId) {
+            message.channel.send(`Tej komendy można używać tylko na kanale <#${config.polecAdminaChannelId}>`);
+            return;
+        }
+
+        if (typeof args[1] === 'undefined') {
+            message.channel.send("Podaj ocene w skali 1 do 5 (napisz samą cyfre a nie np. 2/5)")
+            return;
+        }
+
+        if (isNaN(args[1])) {
+            message.channel.send("Ocena musi być cyfrą!");
+            return;
+        }
+
+        if (!(args[1] < 6 && args[1] > 0)) {
+            message.channel.send("Ocena musi być w skali od 1 do 5 (napisz samą cyfrę np. 2 a nie np. 2/5)");
+        }
+
+        if (typeof args[2] !== 'undefined') {
+
+            var powodPolecenia = "";
+
+            var length = args.length - 1;
+
+            for (let i = 2; i <= length; i++) {
+                powodPolecenia = powodPolecenia + args[i];
+                if (i != length) powodPolecenia = powodPolecenia + " ";
             }
 
-            if (message.channel.id == config.polecAdminaChannelId) {
-                
-                if (typeof args[1] !== 'undefined') {
+            var poleceniaTegoGracza = poleceni.polecenia.find(p => p.ktoPoleca == message.author.id && p.userId == id);
 
-                    
-                    if (!(isNaN(args[1]))) {
+            if (typeof poleceniaTegoGracza === 'undefined') {
 
-                        if (args[1] < 6 && args[1] > 0) {
+                poleceni.polecenia.push({
 
-                            if (typeof args[2] !== 'undefined') {
+                    "userId": id,
+                    "stars": args[1],
+                    "reason": powodPolecenia,
+                    "ktoPoleca": message.author.id
 
-                                var powodPolecenia = "";
+                });
 
-                                var length = args.length - 1;
-                    
-                                for (let i = 2; i <= length; i++) {
-                                    powodPolecenia = powodPolecenia + args[i];
-                                    if (i != length) powodPolecenia = powodPolecenia + " ";
-                                }
-                                
-                                var poleceniaTegoGracza = poleceni.polecenia.find(p => p.ktoPoleca == message.author.id && p.userId == id);
+                var embed = new Discord.MessageEmbed()
+                    .setColor('#34c6eb')
+                    .setTitle("Twoje polecenie zostało przekazane do bazy danych :smiley:")
+                    .setDescription(`**Poleciłeś gracza <@${id}>\nZa: ${powodPolecenia}\nTwoja ocena: ${args[1]}**`)
+                    .setFooter("Pamiętaj! Jendego admina możesz polecić tylko raz.");
 
-                                if (typeof poleceniaTegoGracza === 'undefined') {
-
-                                    poleceni.polecenia.push({
-
-                                        "userId": id,
-                                        "stars": args[1],
-                                        "reason": powodPolecenia,
-                                        "ktoPoleca": message.author.id
-
-                                    });
-
-                                    var embed = new Discord.MessageEmbed()
-                                        .setColor('#34c6eb')
-                                        .setTitle("Twoje polecenie zostało przekazane do bazy danych :smiley:")
-                                        .setDescription(`**Poleciłeś gracza <@${id}>\nZa: ${powodPolecenia}\nTwoja ocena: ${args[1]}**`)
-                                        .setFooter("Pamiętaj! Jendego admina możesz polecić tylko raz.");
-
-                                    message.channel.send(embed);
-
-                                } else {
-                                    message.channel.send("Już raz poleciłeś tego admina!");
-                                }
-
-                                    
-                                
-
-                            } else {
-                                message.channel.send("Powiedz dlaczego chcesz go polecić!");
-                            }
-
-                        } else {
-
-                            message.channel.send("Ocena musi być w skali od 1 do 5 (napisz samą cyfre np. 2 a nie np. 2/5)");
-
-                        }
-
-                    } else {
-                        message.channel.send("ocena musi być cyfrą!");
-                    }
-
-                } else {
-                    message.channel.send("Podaj ocene w skali 1 do 5 (napisz samą cyfre a nie np. 2/5)")
-                    return;
-                }
+                message.channel.send(embed);
 
             } else {
-                message.channel.send(`Tej komendy można używać tylko na kanale <#${config.polecAdminaChannelId}>`);
+                message.channel.send("Już raz poleciłeś tego admina!");
             }
 
         } else {
-            message.channel.send("Podaj prawidłowy nick admina, którego chcesz polecić!");
+            message.channel.send("Powiedz dlaczego chcesz go polecić!");
         }
 
     }
