e8817c2556cb89b654f44d798b417bc5f33bb907
FranekWadas <franiowadas@gmail.com>
2021-06-03 09:44:52 +0200

    replacing words with kreska

--- speakingToBot.js
+++ speakingToBot.js
@@ -1,28 +1,39 @@
 const Discord = require('discord.js');
 
-module.exports = (message, client, channelToSpeakId, StringPrefix, prefix) => {
+module.exports = (message, client, channelToSpeakId, StringPrefix) => {
 
-    if (message.content.startsWith(prefix)) return;
+    if (message.content.startsWith(client.prefix)) return;
 
     if (message.channel.id != channelToSpeakId || message.author.bot) return;
 
     var messageContent = message.content.toLowerCase();
     messageContent = messageContent.replace(/[\\<>@#&!?]/g, "");
 
+    messageContent = messageContent.replace("ą", "a");
+    messageContent = messageContent.replace("ę", "e");
+    messageContent = messageContent.replace("ć", "c");
+    messageContent = messageContent.replace("ł", "l");
+    messageContent = messageContent.replace("ń", "n");
+    messageContent = messageContent.replace("ś", "s");
+    messageContent = messageContent.replace("ż", "z");
+    messageContent = messageContent.replace("ź", "z");
+
     var forFun = client.forFun.find(d => d.guildID == message.guild.id);
     var array = new Array();
 
     var functionObject = forFun.dialogs.find(d => d.message.includes(messageContent));
     
+    if (message.content == "pls meme") {
+      return;
+    }
+
     if (!functionObject) {
         message.channel.send("Przepraszam, ale nie rozumiem :(");
         
         return;
     }
 
-    if (message.content === "pls meme") {
-      return;
-    }
+    
 
     if (functionObject.functionName.startsWith(StringPrefix)) {
 
