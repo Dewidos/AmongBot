ebf9afa54458a55c3f6ee0e8a0abcec33e7bdf33
Dewidos <daw.po@wp.pl>
2021-06-03 14:54:18 +0200

    Done verification system

--- commands/weryfikacja.js
+++ commands/weryfikacja.js
@@ -7,75 +7,81 @@ module.exports = {
 
     var content = "";
 
-    
     var config = client.configFile.find(c => c.guildId == message.guild.id);
 
-    if(typeof config === 'undefined') {
+    if (typeof config === 'undefined') {
       message.channel.send("Błąd konfiguracji!");
       return;
     }
 
-    var weryficationChannel = client.configFile.find(m => m.weryficationChannel == message.channel.id);
+    var verificationChannel = config.verificationChannel;
 
-    if (typeof weryficationChannel === "undefined") {
+    if (typeof verificationChannel === "undefined") {
       message.channel.send("Tej komendy można używać tylko na kanale do weryfikacji!");
       return;
     }
 
     if (config.moderatorRoles.length <= 0) {
-        message.channel.send("Błąd konfiguracji! Nikt nie skonfigurował roli moderatorskich.");
-        return;
+      message.channel.send("Błąd konfiguracji! Nikt nie skonfigurował roli moderatorskich.");
+      return;
+    }
+
+    var hasPermission = false;
+
+    config.moderatorRoles.forEach(role => {
+      if (message.member.roles.cache.has(role)) {
+        hasPermission = true;
       }
+    });
 
-      var hasPermission = false;
-
-      config.moderatorRoles.forEach(role => {
-        if (message.member.roles.cache.has(role)) {
-            hasPermission = true;
-        }
-      });
-
-      if (!hasPermission) {
-        message.channel.send("Nie masz wystarczających uprawnień!");
-        return;
-      }
+    if (!hasPermission) {
+      message.channel.send("Nie masz wystarczających uprawnień!");
+      return;
+    }
 
     if (typeof args[0] !== 'undefined' && args[0] != "") {
-          var length = args.length;
+      var length = args.length;
 
-          for (let i = 0; i <= length; i++) {
-              content = content + args[i];
-              if (i != length) content = content + " ";
-          }
+      for (let i = 0; i <= length; i++) {
+        if (typeof args[i] === 'undefined' || args[i] == "") continue;
+        content = content + args[i];
+        if (i != length) content = content + " ";
       }
-    if (content.endsWith("undefined")) {
-      content = content.replace("undefined", "");
     }
 
     let embed = new Discord.MessageEmbed()
-          .setColor('#27f500')
-          .setTitle("Weryfikacja!")
-          .setDescription(`**${content}**`)
-          .setFooter("Klkiknij ✅, aby się zweryfikować!");
-    
-    let messageEmbed = await message.channel.send(embed);
+      .setColor('#27f500')
+      .setTitle("Weryfikacja!")
+      .setDescription(`**${content}**`)
+      .setFooter("Kliknij ✅, aby się zweryfikować!");
+
+    var messageEmbed = await message.channel.send(embed);
 
     await messageEmbed.react('✅');
 
-    client.on('messageReactionAdd', async (reaction, user) => {
+    var callback = async (reaction, user) => {
 
       if (reaction.message.partial) await reaction.message.fetch();
       if (reaction.partial) await reaction.fetch();
-      if (user.bot) return;
-      if (!reaction.message.guild) return;
+      if (userFromId.partial) await user.fetch();
 
-      if (reaction.message.channel.id == weryficationChannel) {
+      if (reaction.message != messageEmbed) return false;
 
-        reaction.guild.channels.cache.get(message.channel.id).send("siema");
-        
+      if (reaction.emoji.name === '✅') {
+        let member = message.guild.members.cache.get(user.id);
+
+        if (typeof member === 'undefined') {
+          message.channel.send("Wystąpił dziwny błąd!");
+          return false;
+        }
+
+        member.roles.add(config.verificationRole);
       }
 
-    });
+      return false;
+    };
+
+    client.reactionCallbacks.push(callback);
 
   }
 }
