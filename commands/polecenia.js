71083946fddf7b283f5c02629723d101d30dad76
FranekWadas <franiowadas@gmail.com>
2021-06-04 11:56:03 +0200

    osadkkisafdjffsa

--- commands/polecenia.js
+++ commands/polecenia.js
@@ -41,7 +41,7 @@ module.exports = {
                 var nothing = "";
 
                 if (!mamyPolecenie) {
-                    embed.addField(`Ten administrator nie otrzymał jeszcze żadnego polecenia! ${nothing}`, false);
+                    embed.setDescription(`Ten administrator nie otrzymał jeszcze żadnego polecenia!`);
 
                     message.channel.send(embed);
 
