20a708285352ec4ef21b7651acdd092b58c2ca5f
FranekWadas <franiowadas@gmail.com>
2021-06-04 11:13:22 +0200

    test

---
+++ commands/polecenia.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    "name": "polecenia",
+    "description": "Sprawdz polecenia tego admina",
+
+
+    execute(message, args, client) {
+        message.channel.send("cos tam");
+    }
+}
