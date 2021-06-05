187dd73873a7412a003e0674fbb43c44f9b435a1
FranekWadas <franiowadas@gmail.com>
2021-06-05 21:33:03 +0200

    cs ifdsiufjds f

--- commands/pkn.js
+++ commands/pkn.js
@@ -18,34 +18,34 @@ module.exports = {
                 return;
             }
 
-            var tickTacToe = Math.floor(Math.random()*3+1);
+            var tickTacToe = Math.floor(Math.random()*10+1);
 
-            if (tickTacToe = 1) {
+            if (tickTacToe > 0 && tickTacToe < 4) {
                 var botOption = "papier";
-            } else if (tickTacToe = 2) {
+            } else if (tickTacToe > 3 && tickTacToe < 7) {
                 var botOption = "kamień";
-            } else if (tickTacToe = 3) {
+            } else if (tickTacToe > 6) {
                 var botOption = "nożyce";
             }
 
             console.log(botOption);
 
-            if (tickTacToeOptions === 'paper' && botOption === 'kamień') {
+            if (tickTacToeOptions == 'paper' && botOption == 'kamień') {
                 message.channel.send(`**Brawo wygrałeś ${message.author}. Papier bije kamień!**`);
                 return;
-            } else if (tickTacToeOptions === 'scisors' && botOption === 'kamień') {
+            } else if (tickTacToeOptions == 'scisors' && botOption == 'kamień') {
                 message.channel.send(`**Niestety przegrałeś ${message.author}. Kamień bije nożyczki!**`);
                 return;
-            } else if (tickTacToeOptions === 'stone' && botOption === 'nożyce') {
+            } else if (tickTacToeOptions == 'stone' && botOption == 'nożyce') {
                 message.channel.send(`**Brawo wygrałeś ${message.author}. Kamień bije nożyce!**`);
                 return;
-            } else if (tickTacToeOptions === 'paper' && botOption === 'nożyce') {
+            } else if (tickTacToeOptions == 'paper' && botOption == 'nożyce') {
                 message.channel.send(`**Niestety przegrałeś ${message.author}. Nożyce biją papier!**`);
                 return;
-            } else if (tickTacToeOptions === 'stone' && botOption === 'papier') {
+            } else if (tickTacToeOptions == 'stone' && botOption == 'papier') {
                 message.channel.send(`**Niestety przegrałeś ${message.author}. Papier bije kamień!**`);
                 return;
-            } else if (tickTacToeOptions === 'scisors' && botOption === 'papier') {
+            } else if (tickTacToeOptions == 'scisors' && botOption == 'papier') {
                 message.channel.send(`**Brawo wygrałeś ${message.author}. Nożyce bije papier!**`);
                 return;
             }
